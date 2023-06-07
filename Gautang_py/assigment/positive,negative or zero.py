51553bc7f2c99f2359b9f08b001d28c194dd6ece
unknown <mrrajput6780@gmail.com>
2023-06-07 15:00:45 +0530

    python

---
+++ Gautang_py/assigment/positive,negative or zero.py
@@ -0,0 +1,8 @@
+#Write a Python program to check if a number is positive, negegative or zero.
+
+a=int(input("Enter A:"))
+
+if a>0:
+    print("A Is Positive Number")
+else:
+    print("A Is Negative Number")
