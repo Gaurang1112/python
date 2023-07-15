5e67e455ecb41a5ff4744f7085f99077b72b3282
vishalvarmora <vishalvarmora46934@gamil.com>
2023-07-14 23:18:11 -1200

    project

--- Gautang_py/Django_Project/myapp/urls.py
+++ Gautang_py/Django_Project/myapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('contact/',views.contact,name='contact'),
     path('signup/',views.signup,name='signup'),
     path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('change-password/',views.change_password,name='change-password')
 
 ]
