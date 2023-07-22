329af1c388aa78d3874e13a710db358b01eed6c5
vishalvarmora <vishalvarmora46934@gamil.com>
2023-07-21 23:18:49 -1200

    21-7-23

--- Gautang_py/Project/myapp/urls.py
+++ Gautang_py/Project/myapp/urls.py
@@ -3,5 +3,18 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('contact/',views.contact,name='contact'),
+    path('blog/',views.blog,name='blog'),
+    path('cart/',views.cart,name='cart'),
+    path('category/',views.category,name='category'),
+    path('confirmation/',views.confirmation,name='confirmation'),
+    path('elements/',views.elements,name='elements'),
+    path('login/',views.login,name='login'),
+    path('single-blog/',views.single_blog,name='single-blog'),
+    path('single-product/',views.single_product,name='single-product'),
+    path('tracking/',views.tracking,name='tracking'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('signup/',views.signup,name='signup')
+
    
 ]
--- Gautang_py/Project/myapp/views.py
+++ Gautang_py/Project/myapp/views.py
@@ -1,5 +1,84 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def category(request):
+    return render(request,'category.html')
+
+def confirmation(request):
+    return render(request,'confirmation.html')
+
+def elements(request):
+    return render(request,'elements.html')
+
+def login(request):
+    if request.method=="POST":
+        try:
+            user=User.objects.get(email=request.POST['email'])
+            if user.password==request.POST['password']:
+                request.session['email']=user.email
+                request.session['fname']=user.fname
+                return render(request,'index.html')
+            else:
+                msg="Incorrect Password"
+                return render(request,'login.html',{'msg':msg})
+        except:
+            msg="Email Not Registered"
+            return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
+def logout(request):
+    try:
+        del request.session['email']
+        del request.session['fname']
+        return render(request,'login.html')
+    except:
+        return render(request,'login.html')
+
+def single_blog(request):
+    return render(request,'single-blog.html')
+
+def single_product(request):
+    return render(request,'single-product.html')
+
+def tracking(request):
+    return render(request,'tracking.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+def signup(request):
+    if request.method=="POST":
+        try:
+            User.objects.get(email=request.POST['email'])
+            msg="Email Already Registered"
+            return render(request,'signup.html',{'msg':msg})
+        except:
+            if request.POST['password']==request.POST['cpassword']:
+                User.objects.create(
+                        fname=request.POST['fname'],
+                        lname=request.POST['lname'],
+                        email=request.POST['email'],
+                        mobile=request.POST['mobile'],
+                        address=request.POST['address'],
+                        password=request.POST['password'],
+                )
+            msg="User Registered Successfully"
+            return render(request,'signup.html',{'msg':msg})
+        else:
+            msg="Password & Confirm Password Does Not Matched"
+            return render(request,'signup.html',{'msg':msg})
+    else:
+        return render(request,'signup.html')
