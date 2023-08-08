c7b16268a2e3d29a295d053ada61573b0e58a1db
rahulvankar <vankarrahul2710@gmail.com>
2023-08-08 16:45:34 +0530

    python

--- Gautang_py/frutika-master/myapp/urls.py
+++ Gautang_py/frutika-master/myapp/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('contact/',views.contact,name='contact'),
+    path('page/',views.page,name='page'),
+    path('about/',views.about,name='about'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('news/',views.news,name='news'),
+    path('shop/',views.shop,name='shop'),
+    path('single-news/',views.single_news,name='single-news'),
+    path('account/',views.account,name='account'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login'),
 ]
--- Gautang_py/frutika-master/myapp/views.py
+++ Gautang_py/frutika-master/myapp/views.py
@@ -4,3 +4,37 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def page(request):
+    return render(request,'page.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+def news(request):
+    return render(request,'news.html')
+
+def shop(request):
+    return render(request,'shop.html')
+
+def single_news(request):
+    return render(request,'single-news.html')
+
+def account(request):
+    return render(request,'account.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
+
